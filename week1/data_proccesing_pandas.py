ecc25346bf8af2329fd838b68bc9324aca6e5de8
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-05-14 03:37:30 +0300

    task finished

--- week1/data_proccesing_pandas.py
+++ week1/data_proccesing_pandas.py
@@ -36,8 +36,43 @@ def count_male(data):
             ans+=1
     return ans
 
+def count_survived(data):
+    ans = 0
+    for str in data['Survived']:
+        if (str == 1):
+            ans += 1
+    return ans
+
+def count_first_class(data):
+    ans = 0
+    for str in data['Pclass']:
+        if (str == 1):
+            ans += 1
+    return ans
+
+def avg_age(data):
+    ans = 0
+    count = 0
+    ages = []
+    for str in data['Age']:
+        if (str > 0):
+            ans += str
+            count += 1
+            ages.append(str)
+    ages.sort()
+    print(ages[int(count/2)])
+    return ans/count
+
 def main():
     data = import_data('PassengerId')
+    print("Бабы и мужики")
     print(count_male(data))
     print(len(data['Sex'])-count_male(data))
+    print("Не сдохло")
+    print( 100*count_survived(data)/len(data))
+    print("Доля первого класса")
+    print(100*count_first_class(data)/len(data))
+    print("Средний возраст и медиана ")
+    print(avg_age(data))
+    print(data.corr(method='pearson'))
 main()
