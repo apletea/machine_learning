c44707557a00e362252593f59ea1a3a5cccaa966
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-05-17 00:10:15 +0300

    week2 task 2

--- week2/task_2.py
+++ week2/task_2.py
@@ -1,2 +1,30 @@
 import numpy as np
 import pandas
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.datasets import load_boston
+from sklearn.preprocessing import scale
+from sklearn.cross_validation import KFold
+from sklearn import cross_validation
+
+def main():
+    #load data set
+    set = load_boston()
+    #define X and y
+    X = set['data']
+    y = set['target']
+    #seting to equals
+    attributes =scale(X)
+    #cross validator
+    kfolgen = KFold(n = y.shape[0],n_folds=5,shuffle=True,random_state=42)
+
+    for p in np.linspace(1, 10, num=200):
+        #clasifier
+        clf = KNeighborsRegressor(n_neighbors=5, weights='distance', metric='minkowski', p=p)
+        #cross validation score
+        sc = cross_validation.cross_val_score(clf, X, y, cv=kfolgen, scoring='mean_squared_error')
+        #average
+        msc = np.mean(sc)
+        #print average
+        print(msc)
+
+main()
