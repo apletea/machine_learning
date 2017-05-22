a4473c6df58333abba62722d713a115a0d5bed91
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-05-22 16:53:29 +0300

    first perceptron

--- week2/task_3.py
+++ week2/task_3.py
@@ -12,11 +12,47 @@ def perceptron_example():
     predictions = clf.predict(X)
     print(predictions)
     print(clf.predict([[1110,280]]))
-perceptron_example()
 
-def accuracy_example():
+
+def scaller_example():
   scaller = StandardScaler()
   X_train = np.array([[100.0, 2.0], [50.0, 4.0], [70.0, 6.0]])
   X_test = np.array([[90.0, 1], [40.0, 3], [60.0, 4]])
   X_train_scaled = scaller.fit_transform(X_train)
   X_test_scaled = scaller.transform(X_test)
+
+
+def main():
+    #loading data
+    data_test = pandas.read_csv('perceptron-test.csv', header=None)
+    data_train = pandas.read_csv('perceptron-train.csv', header=None)
+    #parseing data
+    X_train = data_train.loc[:,1:2]
+    Y_train = data_train[0]
+
+    X_test  = data_test.loc[:,1:2]
+    Y_test = data_test[0]
+    #init and train modekl
+    clf = Perceptron(random_state=241)
+    clf.fit(X_train,Y_train)
+    #predict for objects
+    y_pred = clf.predict(X_test)
+    #count score
+    fist_ans = (accuracy_score(Y_test, y_pred))
+    #init new perceptron
+    clf = Perceptron(random_state=241)
+    #init scaller
+    scaller = StandardScaler()
+    #scale data
+    X_train_scalled = scaller.fit_transform(X_train)
+    X_test_scalled = scaller.transform(X_test)
+    #fit new model
+    clf.fit(X_train_scalled,Y_train)
+    #predict for new objects
+    y_pred = clf.predict(X_test_scalled)
+    #find new accuracy
+    second_ans = accuracy_score(Y_test,y_pred)
+    print(second_ans-fist_ans)
+
+
+main()
