9a85b14b9e58974eca894fbf767e356884ee2989
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-05-26 17:35:10 +0300

    3.1

---
+++ week3/task_1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas
+from sklearn.svm import SVC
+
+
+
+
+def main():
+    data = pandas.read_csv('svm-data.csv', header=None)
+
+    y=data[0]
+    X = data.loc[:,1:2]
+
+    clf = SVC(C=100000, random_state=241)
+    clf.fit(X,y)
+
+    print(clf.support_vectors_)
+    print(clf.support_)
+main()
