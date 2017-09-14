50421fb6dd84d15397fe806562cf2d845f60b6b9
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-09-14 15:42:02 +0300

    Update main.py

--- aplied machine learning/week3/main.py
+++ aplied machine learning/week3/main.py
@@ -1,15 +1,16 @@
 import pandas
 import os
 from sets import  Set
+
+from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
-
-source_file = '/home/apletea/PycharmProjects/untitled1/posters'
-true_file = '/home/apletea/PycharmProjects/untitled1/true/'
-false_file='/home/apletea/PycharmProjects/untitled1/false/'
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.gaussian_process.kernels import RBF
 labeled_data = pandas.read_csv('train.csv', index_col='Id')
 
 
@@ -41,13 +42,19 @@ ratings_le.fit(list(ratings))
 ratings_a = ratings_le.transform(ratings_a)
 
 labeled_data['Language'],labeled_data['Country'],labeled_data['Rating'] = languages_a,countries_a,ratings_a
-print labeled_data
+
 data = labeled_data.iloc[0:3635, 1:26]
+#data = preprocessing.scale(data)
+print data
 labels = labeled_data.iloc[0:3635, :1]
-train_data, test_data, train_labeles, test_labeles = train_test_split(data,labels,train_size=0.8,random_state=250)
+train_data, test_data, train_labeles, test_labeles = train_test_split(data,labels,train_size=0.9,random_state=250)
 
-
-
-clf = MLPClassifier()
+clf = MLPClassifier(activation='relu',batch_size=200,random_state=247)
+#clf = DecisionTreeClassifier(random_state=247)
+#clf = svm.SVC(random_state=247)
+#clf = QuadraticDiscriminantAnalysis()
+#clf = GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True)
 clf.fit(train_data,train_labeles.values.ravel())
+print clf.predict_proba(test_data)[:,:1]
+
 print clf.score(test_data,test_labeles)
