67fd91b2938a2c101b8a31b03ed8183f8dd4059d
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-12-27 15:03:18 +0300

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,20 +1,25 @@
-    ip1 = PReLU(name='ip1')(last)
-    ip2 = Dense(num_classes)(ip1)
-    ip2 = Activation('sigmoid')(ip2)
-    lambda_c = 0.2
+def get_model_best_cl(num_classes):
+    vec_dim = 128
+    model = load_model('models/{}.hdf5'.format('inference_Xception_1.2_remote_kkk'), custom_objects={'tf':tf})
+    last = model.layers[-5].output
+    last = Dense(vec_dim)(last)
+#    last = Lambda(lambda x: K.tf.nn.softmax(x))(last)
+#Center loss
+    ip1 = model.get_layer('ip1').output
+    ip2 = Dense(num_classes)(ip1) 
+    ip2 = Activation('softmax')(ip2)
     emb_inputs = []
-    for i in num_classes:
-        emb_inputs.append(Input(shape=(1,), name='input_{}'.format(str(i+2))))
-#        input_target      = Input(shape=(1,), name='input_3') # single value ground truth labels as inputs
-#        input_target_inv  = Input(shape=(1,), name='input_4') 
-    center_model_iput = Input(shape=(1,), name='input_5') 
+    for i in range(num_classes):
+        emb_inputs.append(Input(shape=(1,), name='input_{}'.format(str(i+6))))
+
+    center_model_iput = Input(shape=(1,), name='input_2') 
     centers = Embedding(num_classes,vec_dim)(center_model_iput)
     centers = Model(center_model_iput, centers)
     centers_arr = []
-    for i in num_classes:
-        centers_arr.append(centers[emb_inputs[i]])
-    cur_cen   = centers(input_target)
-    other_cen = centers(input_target_inv)
-    l2_loss = Lambda(lambda x: K.sum(K.square(x[0]-x[1][:,0])/(K.square(x[0]-x[2][:,0])+1),keepdims=True),name='l2_loss')([ip1, cur_cen, other_cen])
-    model_centerloss = Model(inputs=[model.input,input_target,input_target_inv],outputs=[ip2,l2_loss]) 
-    plot_model(model_centerloss, to_file='model.png')  
+    for i in range(num_classes):
+        centers_arr.append(centers(emb_inputs[i]))
+    l2_loss = Lambda(lambda x: K.sum(K.square(x[0]-x[1][:,0])+1/(K.sum(K.square([x[0]-x[i][:,0] for i in range(2,len(x))]))+1),keepdims=True),name='l2_loss')([ip1] + centers_arr)
+    model_centerloss = Model(inputs=[model.input] + emb_inputs, outputs=[ip2,l2_loss]) 
+#   for index in range(len(model.layers)-7):
+#       model_centerloss.layers[index].trainable=False
+    return model_centerloss
